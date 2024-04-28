76ed4e6de114fc14b18657cade876eea0cad4ae7
Barragan <l21020480@veracruz.tecnm.mx>
2024-04-27 20:55:19 -0600

    Version 1.4 - modificacion a clase HiloBalaAzul

--- Proyecto_Hilos_TAPEJ2024/src/main/java/hilos/HiloBalaAzul.java
+++ Proyecto_Hilos_TAPEJ2024/src/main/java/hilos/HiloBalaAzul.java
@@ -3,5 +3,58 @@ package hilos;
 import java.awt.*;
 
 public class HiloBalaAzul extends Thread {
+    private int x, y, incX, incY;
+    private final int RADIO_BALA = 40;
+    private Graphics g;
+
+    public HiloBalaAzul(Graphics g, int px, int py) {
+        this.g = g;
+        x = px;
+        y = py;
+        incX = 2; // Velocidad más lenta que las balas rojas
+        incY = 1; // Velocidad más lenta que las balas rojas
+    }
+
+    public void run() {
+        while (true) {
+            mover();
+            try {
+                Thread.sleep(15);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public int getRadio() {
+        return RADIO_BALA;
+    }
+
+    private void mover() {
+        // Lógica del movimiento de la bala azul
+        x += incX;
+        y += incY;
+
+        // La bala rebota si sale de la ventana
+        if (x - RADIO_BALA <= -50 || x + RADIO_BALA >= 900) {
+            incX *= -1;
+        }
+        if (y - RADIO_BALA <= -50 || y + RADIO_BALA >= 1000) {
+            incY *= -1;
+        }
+    }
+
+    public void dibujar(Graphics g) {
+        g.setColor(Color.BLUE);
+        g.fillOval(x - RADIO_BALA, y - RADIO_BALA, 2 * RADIO_BALA, 2 * RADIO_BALA);
+    }
 
 }
