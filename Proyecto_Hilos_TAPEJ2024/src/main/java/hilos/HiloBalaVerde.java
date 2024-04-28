8710b66c2dce3503c7ebe100e136032cc5fe66d5
Juan Sanchez <l22020825@veracruz.tecnm.mx>
2024-04-27 22:14:56 -0600

    Version 1.8 - correcion de errores

--- Proyecto_Hilos_TAPEJ2024/src/main/java/hilos/HiloBalaVerde.java
+++ Proyecto_Hilos_TAPEJ2024/src/main/java/hilos/HiloBalaVerde.java
@@ -1,63 +1,69 @@
-package Hilos;
+package hilos;
 
-private int x, y, incX;
-private final int ANCHO_BALA = 120;
-private final int ALTO_BALA = 15;
-private Graphics g;
-private int ventanaAncho;
-private int ventanaAlto;
+import java.awt.*;
 
-public HiloBalaVerde(Graphics g, int ventanaAncho, int ventanaAlto, int direccion) {
-    this.g = g;
-    this.ventanaAncho = ventanaAncho;
-    this.ventanaAlto = ventanaAlto;
+public class HiloBalaVerde extends Thread {
+
+    private int x, y, incX;
+    private final int ANCHO_BALA = 120;
+    private final int ALTO_BALA = 15;
+    private Graphics g;
+    private int ventanaAncho;
+    private int ventanaAlto;
+
+    public HiloBalaVerde(Graphics g, int ventanaAncho, int ventanaAlto, int direccion) {
+        this.g = g;
+        this.ventanaAncho = ventanaAncho;
+        this.ventanaAlto = ventanaAlto;
+
+        y = (int) (Math.random() * (ventanaAlto - ALTO_BALA));
 
 
-    y = (int) (Math.random() * (ventanaAlto - ALTO_BALA));
-
-    if (direccion == 0) { // Desde la izquierda
-        x = -ANCHO_BALA;
-        incX = 2; // Velocidad
-    } else { // Desde la derecha
-        x = ventanaAncho;
-        incX = -2; // Velocidad
-    }
-}
-
-public void run() {
-    while (x >= -ANCHO_BALA && x <= ventanaAncho) { // Mientras la bala esté dentro de la ventana
-        mover();
-        try {
-            Thread.sleep(15);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
+        if (direccion == 0) { // Desde la izquierda
+            x = -ANCHO_BALA;
+            incX = 2; // Velocidad
+        } else { // Desde la derecha
+            x = ventanaAncho;
+            incX = -2; // Velocidad
         }
     }
-}
 
-private void mover() {
-    // Mover la bala en la dirección determinada
-    x += incX;
-}
+    public void run() {
+        while (x >= -ANCHO_BALA && x <= ventanaAncho) { // Mientras la bala esté dentro de la ventana
+            mover();
+            try {
+                Thread.sleep(15);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
 
-public void dibujar(Graphics g) {
-    g.setColor(Color.GREEN);
-    g.fillRect(x, y, ANCHO_BALA, ALTO_BALA);
-}
+    private void mover() {
+        // mover la bala en la dirección determinada
+        x += incX;
+    }
 
-public int getX() {
-    return x;
-}
+    public void dibujar(Graphics g) {
+        g.setColor(Color.GREEN);
+        g.fillRect(x, y, ANCHO_BALA, ALTO_BALA);
+    }
 
-public int getY() {
-    return y;
-}
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
 
 
-public int getAnchoBala() {
-    return ANCHO_BALA;
-}
+    public int getAnchoBala() {
+        return ANCHO_BALA;
+    }
 
-public int getAltoBala() {
-    return ALTO_BALA;
+
+    public int getAltoBala() {
+        return ALTO_BALA;
+    }
 }
